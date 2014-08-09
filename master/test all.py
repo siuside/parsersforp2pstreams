ff789816ccb718564c4f2ce49d073831c0f1676f
smokeythejunglist@gmail.com <smokeythejunglist@gmail.com@c4819c30-b6aa-db62-3cfa-30009adc26c2>
2014-08-09 23:21:17 +0000

    :) hopefully

---
+++ master/test all.py
@@ -0,0 +1,10 @@
+﻿# -*- coding: utf-8 -*-
+import xbmc,urllib
+
+all_modules = [ 'https://parsersforp2pstreams.googlecode.com/svn/trunk/%20parsersforp2pstreams/arenavision.tar.gz','https://parsersforp2pstreams.googlecode.com/svn/trunk/%20parsersforp2pstreams/livefootballaol.tar.gz','https://parsersforp2pstreams.googlecode.com/svn/trunk/%20parsersforp2pstreams/livefootballvideo.tar.gz','https://parsersforp2pstreams.googlecode.com/svn/trunk/%20parsersforp2pstreams/livefootballws.tar.gz','https://parsersforp2pstreams.googlecode.com/svn/trunk/%20parsersforp2pstreams/onetorrenttv.tar.gz','https://parsersforp2pstreams.googlecode.com/svn/trunk/%20parsersforp2pstreams/rojadirecta.tar.gz','https://parsersforp2pstreams.googlecode.com/svn/trunk/%20parsersforp2pstreams/sopcastucoz.tar.gz','https://parsersforp2pstreams.googlecode.com/svn/trunk/%20parsersforp2pstreams/torrenttvruall.tar.gz','https://parsersforp2pstreams.googlecode.com/svn/trunk/%20parsersforp2pstreams/torrenttvrusports.tar.gz','https://parsersforp2pstreams.googlecode.com/svn/trunk/%20parsersforp2pstreams/wiziwig.tar.gz']
+
+for parser in all_modules:
+    xbmc.executebuiltin('XBMC.RunPlugin("plugin://plugin.video.p2p-streams/?mode=405&name=p2p&url=' + urllib.quote(parser) + '")')
+    xbmc.sleep(1000)
+
+xbmc.executebuiltin("Notification(%s,%s,%i,%s)" % ('P2P-Streams', "All parsers imported",1,''))

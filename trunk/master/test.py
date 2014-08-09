7d3ae69f62229ff57133b2cde828ae5fd999f2a4
smokeythejunglist@gmail.com <smokeythejunglist@gmail.com@c4819c30-b6aa-db62-3cfa-30009adc26c2>
2014-08-09 17:02:31 +0000

    ' missing

--- trunk/master/test.py
+++ trunk/master/test.py
@@ -1,7 +1,7 @@
-﻿# -*- coding: utf-8 -*-
+ # -*- coding: utf-8 -*-
 import xbmc,urllib
 
-all_modules = [ 'https://code.google.com/p/parsersforp2pstreams/source/browse/branches/P2P-STREAMS-Parsers/1Torrent.tv/onetorrenttv.tar.gz','https://code.google.com/p/parsersforp2pstreams/source/browse/branches/P2P-STREAMS-Parsers/Arenavision.in/arenavision.tar.gz',https://code.google.com/p/parsersforp2pstreams/source/browse/branches/P2P-STREAMS-Parsers/Livefootball.ws/livefootballws.tar.gz']
+all_modules = [ 'https://code.google.com/p/parsersforp2pstreams/source/browse/branches/P2P-STREAMS-Parsers/1Torrent.tv/onetorrenttv.tar.gz','https://code.google.com/p/parsersforp2pstreams/source/browse/branches/P2P-STREAMS-Parsers/Arenavision.in/arenavision.tar.gz','https://code.google.com/p/parsersforp2pstreams/source/browse/branches/P2P-STREAMS-Parsers/Livefootball.ws/livefootballws.tar.gz']
 
 for parser in all_modules:
     xbmc.executebuiltin('XBMC.RunPlugin("plugin://plugin.video.p2p-streams/?mode=405&name=p2p&url=' + urllib.quote(parser) + '")')
